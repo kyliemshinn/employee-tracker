8a35b29cb3750f3b76a50233cf218ae5f3cabdf1
Kylie Shinn <kyliemshinn@gmail.com>
2022-04-30 13:15:16 -0700

    create outline of code structure for a clear plan of project layout

---
+++ server.js
@@ -0,0 +1,30 @@
+// require variables needed for code to run
+
+// connection for sql to server.js file
+
+// inquirer prompts for employees- will be multiple different 
+    // what would you like to do ?
+        // subset of questions to populate schema tables
+            // view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+            
+
+    // after select all deps, show formatted table with dep names and id
+    
+    // view all roles then I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+
+    //view all employees- show formatted table w employee data- employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+    // add a department- prompted to enter the name of the department and that department is added to the database
+
+    // add a role- prompted to enter the name, salary, and department for the role and that role is added to the database
+
+    // add an employee- prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+
+    // update an employee role-  prompted to select an employee to update and their new role and this information is updated in the database 
+
+
+
+
+
+
+            
