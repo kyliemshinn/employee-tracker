f61d86a0f4e0fbb605e07f4ba10872142855356e
Kylie Shinn <kyliemshinn@gmail.com>
2022-04-30 15:01:42 -0700

    add initial prompt with inquirer, set conditional statement for if each is picked, add skeleton functions for each conditional

--- server.js
+++ server.js
@@ -14,25 +14,77 @@ const db = mysql.createConnection(
   );
 // inquirer prompts for employees- will be multiple different 
     // what would you like to do ?
-        // subset of questions to populate schema tables
-            // view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+    // subset of questions to populate schema tables
+        // view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+    function initPrompt() {
+        const startQuestion = [{
+          type: "list",
+          name: "choice",
+          message: "what would you like to do?",
+          loop: false,
+          choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+        }]
 
-
-    // after select all deps, show formatted table with dep names and id
+    .then(function(choices) {
+        switch (choices.choice) {
+            case "view all departments":
+              viewDepts();
+            break;
     
+          case "view all roles":
+              viewRoles();
+            break;
+          case "view all employees":
+              viewEmployees();
+            break;
+          
+          case "add a department":
+                addDept();
+              break;
+
+          case "add a role":
+                addRole();
+              break;
+      
+            case "add an employee":
+                addEmployee();
+              break;
+      
+            case "update an employee role":
+                updateEmployee();
+              break;
+    
+            }
+    })
+}
+    // after select all deps, show formatted table with dep names and id
+    const viewDepts =() => {
+
+    }
     // view all roles then I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+    const viewRoles =() => {
 
+    }
     //view all employees- show formatted table w employee data- employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+    const viewEmployees =() => {
 
+    }
     // add a department- prompted to enter the name of the department and that department is added to the database
+    const addDept =() => {
 
+    }
     // add a role- prompted to enter the name, salary, and department for the role and that role is added to the database
+    const addRole =() => {
 
+    }
     // add an employee- prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+    const addEmployee =() => {
 
+    }
     // update an employee role-  prompted to select an employee to update and their new role and this information is updated in the database 
+    const updateEmployee =() => {
 
-
+    }
 
 
 
