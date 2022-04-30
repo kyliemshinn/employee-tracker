a40ae8b6568fc34b7d088c9791f2a6d25c9a0b09
Kylie Shinn <kyliemshinn@gmail.com>
2022-04-30 14:27:02 -0700

    update seeds.sql values

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,14 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
-USE employees_db
+USE employees_db;
 
 -- department table
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 
-)
+);
 
 
 -- role table
@@ -16,18 +16,23 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id)
-)
+    department_id INT,
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id)
+);
 
 
 
 -- employee table
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id IN NOT NULL,
-  manager_id INT NULL,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) 
+  REFERENCES role(id),
+  FOREIGN KEY (manager_id) 
+  REFERENCES employee(id)
+);
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,34 @@
 -- DEPARTMENT NAMES- ENGINEER, FINANCE, LEGAL, SALES
+INSERT INTO department (name)
+VALUES  ("Engineering"),
+        ("Finance"),
+        ("Legal"),
+        ("Sales");
+       
 
 -- ROLE TITLE- SALESLEAD; dep sales, SALESPERSON; dep sales, LEAD ENGINEER dep engineering, SOFTWARE ENGINEER; dep engineering, ACCOUNT MANAGER dep finance, ACCOUNTANT; dep finance, LEGAL TEAM LEAD; dep legal, LAYWER; dep legal
     --associated salary in order 100k, 80k, 150k, 120k, 160k, 125k, 250k, 190k
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Saleslead", 100000, 4),
+        ("Salesperson", 80000, 4),
+        ("Lead Engineer", 150000, 1),
+        ("Software Engineer", 120000, 1),
+        ("Account Manager", 160000, 2),
+        ("Accountant", 125000, 2),
+        ("Legal Team Lead", 250000, 3),
+        ("Laywer", 190000, 3);
 
 -- show first name, last name title, dep, salary manager
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Tony", "Stark", 4, 1),
+        ("Steve", "Rodgers", 4, NULL),
+        ("Thor", "Odinson", 1, 2),
+        ("Loki", "Odinson", 1, NULL),
+        ("Wanda", "Maximoff", 2, 3),
+        ("James", "Rhodes", 2, NULL),
+        ("Natasha", "Rominoff", 3, 4),
+        ("Samuel", "Wilson", 3, NULL);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee; 
